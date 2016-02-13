717ad70e2b3e5ad12f6628dcd314fce417c76692
lizhen325 <zhenli336@gmail.com>
2016-02-13 22:42:32 +1300

    Add RoomInfo class into model which based on RoomInfo table 	new file:   CMS/ItcastCaterModel/RoomInfo.cs

---
+++ CMS/ItcastCaterModel/RoomInfo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItcastCater.Model
+{
+    //RoomInfo table 
+
+    //RoomId, RoomName, RoomType, RoomMinimumConsumer
+    //RoomMaxConsumer, IsDefault, DelFlag, SubTime, SubBy
+    public class RoomInfo
+    {
+        private int _roomId;
+
+        public int RoomId
+        {
+            get { return _roomId; }
+            set { _roomId = value; }
+        }
+        private string _roomName;
+
+        public string RoomName
+        {
+            get { return _roomName; }
+            set { _roomName = value; }
+        }
+        private int _roomType;
+
+        public int RoomType
+        {
+            get { return _roomType; }
+            set { _roomType = value; }
+        }
+        private decimal _roomMinimumConsumer;
+
+        public decimal RoomMinimumConsumer
+        {
+            get { return _roomMinimumConsumer; }
+            set { _roomMinimumConsumer = value; }
+        }
+        private int _roomMaxConsumer;
+
+        public int RoomMaxConsumer
+        {
+            get { return _roomMaxConsumer; }
+            set { _roomMaxConsumer = value; }
+        }
+        private int _isDefault;
+
+        public int IsDefault
+        {
+            get { return _isDefault; }
+            set { _isDefault = value; }
+        }
+        private int _delFlag;
+
+        public int DelFlag
+        {
+            get { return _delFlag; }
+            set { _delFlag = value; }
+        }
+        private DateTime _subTime;
+
+        public DateTime SubTime
+        {
+            get { return _subTime; }
+            set { _subTime = value; }
+        }
+        private int _subBy;
+
+        public int SubBy
+        {
+            get { return _subBy; }
+            set { _subBy = value; }
+        }
+    }
+}
